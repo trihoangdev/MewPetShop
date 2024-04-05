e797642fe8938a2adc8d0c8caedc122cbc4584ce
trihoangdev <louishoang.it@gmail.com>
2024-04-05 16:44:29 +0700

    Sắp xếp thứ tự các chức năng

--- js/login.js
+++ js/login.js
@@ -1,21 +1,70 @@
+//Chức năng đăng nhập
+//Sự kiện đăng nhập tài khoản
+btnLogin.addEventListener("click", function () {
+  var username = accountFieldLogin.value;
+  var password = "";
+  var success = false;
+  if (passwordFieldLogin.tagName.toLowerCase() === "input")
+    password = passwordFieldLogin.value;
+  else password = passwordFieldLogin.textContent;
+  accounts.forEach((element) => {
+    if (element.username == username && element.pass == password) {
+      success = true;
+      return;
+    }
+  });
+  if (success) {
+    alert("Đăng nhập thành công!");
+    window.location.href = "../index.html";
+  } else alert("Tài khoản hoặc mật khẩu không chính xác!");
+});
 //Lấy danh sách các tài khoản hiện có trong Local Storage
 var accounts = JSON.parse(localStorage.getItem("accounts"));
 if (accounts == null) {
   accounts = [];
 }
+//Tham chiếu đến các thẻ
+const passwordFieldLogin = document.querySelector(
+  '.login-form .input-box input[type="password"]'
+);
+const accountFieldLogin = document.getElementById("account-field-login");
+const errorAccLogin = document.getElementById("error-acc-login");
+const errorPassLogin = document.getElementById("error-pass-login");
+const errorAccRegister = document.getElementById("error-acc-register");
+const btnLogin = document.getElementById("btn-login");
+
+//Sự kiện kiểm tra mật khẩu có đúng định dạng không, nếu sai báo lỗi
+passwordFieldLogin.addEventListener("change", function () {
+  checkPassword(errorPassLogin, passwordFieldLogin);
+});
+
+//Kiểm tra định dạng của password
+function checkPassword(error, field) {
+  var pass = field.value;
+  if (!isPasswordCorrect(pass)) {
+    error.innerHTML =
+      "Mật khẩu phải có ít nhất 8 ký tự, 1 chữ cái, 1 chữ số, một ký tự đặc biệt";
+    error.parentNode.style.marginBottom = "60px";
+  } else {
+    error.innerHTML = "";
+    error.parentNode.style.marginBottom = "30px";
+  }
+}
+//Kiểm tra mật khẩu đúng định dạng
+function isPasswordCorrect(pass) {
+  var regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?@ "]).{8,}$/;
+  return regex.test(pass);
+}
 
 //Tham chiếu đến các thẻ
-const registerLink = document.getElementById("register-link");
-const loginLink = document.getElementById("login-link");
 const showHidePasswordLogin = document.getElementById("show-hide-pass-login");
+const loginLink = document.getElementById("login-link");
+const registerLink = document.getElementById("register-link");
 const showHidePasswordRegister = document.getElementById(
   "show-hide-pass-register"
 );
 const showHidePasswordRegisterAgain = document.getElementById(
   "show-hide-pass-register-again"
-);
-const passwordFieldLogin = document.querySelector(
-  '.login-form .input-box input[type="password"]'
 );
 const passwordFieldRegister = document.querySelector(
   '.register-form .input-box input[type="password"]'
@@ -24,16 +73,12 @@ const passwordFieldAgainRegister = document.getElementById(
   "input-password-again"
 );
 const accountFieldRegister = document.getElementById("account-field-register");
-const accountFieldLogin = document.getElementById("account-field-login");
-const errorAccLogin = document.getElementById("error-acc-login");
-const errorPassLogin = document.getElementById("error-pass-login");
-const errorAccRegister = document.getElementById("error-acc-register");
+
 const errorPassRegister = document.getElementById("error-pass-register");
 const errorPassAgainRegister = document.getElementById(
   "error-pass-again-register"
 );
 const btnRegister = document.getElementById("btn-register");
-const btnLogin = document.getElementById("btn-login");
 
 //Sự kiện kích hoạt lật thẻ
 registerLink.addEventListener("click", function (e) {
@@ -140,27 +185,6 @@ passwordFieldAgainRegister.addEventListener("change", function () {
   checkPassword(errorPassAgainRegister, passwordFieldAgainRegister);
   checkPasswordMatches(passwordFieldRegister, passwordFieldAgainRegister);
 });
-passwordFieldLogin.addEventListener("change", function () {
-  checkPassword(errorPassLogin, passwordFieldLogin);
-});
-
-//Kiểm tra định dạng của password
-function checkPassword(error, field) {
-  var pass = field.value;
-  if (!isPasswordCorrect(pass)) {
-    error.innerHTML =
-      "Mật khẩu phải có ít nhất 8 ký tự, 1 chữ cái, 1 chữ số, một ký tự đặc biệt";
-    error.parentNode.style.marginBottom = "60px";
-  } else {
-    error.innerHTML = "";
-    error.parentNode.style.marginBottom = "30px";
-  }
-}
-//Kiểm tra mật khẩu đúng định dạng
-function isPasswordCorrect(pass) {
-  var regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?@ "]).{8,}$/;
-  return regex.test(pass);
-}
 
 //Mật khẩu có trùng khớp ko
 function checkPasswordMatches(pass, passAgain) {
@@ -201,24 +225,4 @@ btnRegister.addEventListener("click", function () {
   //Ghi vào LocalStorage
   localStorage.setItem("accounts", JSON.stringify(accounts));
   alert("Đăng ký thành công! Vui lòng đăng nhập để mua hàng!");
-});
-
-//Sự kiện đăng nhập tài khoản
-btnLogin.addEventListener("click", function () {
-  var username = accountFieldLogin.value;
-  var password = "";
-  var success = false;
-  if (passwordFieldLogin.tagName.toLowerCase() === "input")
-    password = passwordFieldLogin.value;
-  else password = passwordFieldLogin.textContent;
-  accounts.forEach((element) => {
-    if (element.username == username && element.pass == password) {
-      success = true;
-      return;
-    }
-  });
-  if (success) {
-    alert("Đăng nhập thành công!");
-    window.location.href = "../index.html";
-  } else alert("Tài khoản hoặc mật khẩu không chính xác!");
 });
